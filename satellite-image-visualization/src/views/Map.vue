66088764ee091cfcf39638c1e73f0c668578bafb
fullop2 <taehyun.park.fullop@gmail.com>
2021-05-17 13:07:08 +0900

    ADD Feature object detection
    - 물체 인식 정보 시각화 기능 구현
     - mock server에 데이터 요청(axios).
     - PNG, GEOJSON 파일 수신
     - 물체 인식 정보를 벡터레이어에 넣어서 시각화
     - 위성 사진을 일정 줌 레벨 이상에서 출력
     - 위성 사진에 대한 border 출력

---
+++ satellite-image-visualization/src/views/Map.vue
@@ -0,0 +1,179 @@
+<template>
+  <div>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import 'ol/ol.css';
+import Map from 'ol/Map' ;
+import View from 'ol/View';
+
+import {Image as ImageLayer, Vector as VectorLayer, Tile as TileLayer} from 'ol/layer';
+
+import {XYZ, ImageStatic as Static} from 'ol/source';
+import VectorSource from 'ol/source/Vector';
+
+import {fromLonLat} from 'ol/proj';
+import {getCenter} from 'ol/extent';
+
+import {Fill, Stroke, Style} from 'ol/style';
+import Feature from 'ol/Feature';
+import Polygon from 'ol/geom/Polygon';
+
+import GeoJSON from 'ol/format/GeoJSON';
+
+import axios from 'axios';
+
+
+export default {
+  name: 'Map',
+  props:{
+    map_key: { type:String, default: '8xehacjegYD1ARG8VucP'},
+    position: {type:Array, default:null},
+    zoom: {type:Number, default: 0},
+    info: {
+      type:Object,
+      default:{
+        type:'plane',
+        date:'20210121'
+      }
+    }
+  },
+  watch:{
+    info(){
+      if(this.info != null){
+        this.load_geo();
+        this.load_img();
+      }
+    }
+  },
+  data(){
+    return {
+      view: null,
+      vector: null,
+      map: null,
+      print_info:{
+        img:null,
+        geo:null
+      },
+      minZoomFeatureInfo: 10
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods:{
+    init(){
+      let tileLayer = new TileLayer({
+        source: new XYZ({
+          url: 'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=' + this.map_key,
+        }),
+        visible: true
+      });
+
+      this.view= new View({
+        center:this.position,
+        zoom:this.zoom
+      });
+
+      this.map = new Map({
+        layers: [tileLayer],
+        view: this.view,
+        target: 'map'
+      });
+      
+      if(this.info != null){
+        this.load_geo();
+        this.load_img();
+      }
+
+      this.map.on('moveend', ()=>{
+        this.$emit('changePosition',this.view.getCenter(), this.view.getZoom());
+      });
+    },
+
+    load_img(){
+      const map_dir = `http://localhost:3000/map/${this.info.date}/${this.info.type}`;
+
+      const TL = fromLonLat([126.42289638519286, 37.443824358499114]);
+      const BR = fromLonLat([126.46053314208984, 37.47451749927094]);
+      var extent = TL.concat(BR);
+
+      function getBorder(TL,BR){
+        return [[
+          TL,
+          [BR[0],TL[1]],
+          BR,
+          [TL[0],BR[1]],
+          TL
+        ]];
+      }
+
+      function blueBoldStyle(){
+        return new Style({
+          stroke: new Stroke({
+            color: 'blue',
+            width: 5,
+          }),
+          fill: new Fill({
+            color: 'rgba(0, 0, 255, 0.1)',
+          }),
+        })
+      }
+
+      let imageOutlineLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [new Feature(new Polygon(getBorder(TL,BR)))]
+        }),
+        style: blueBoldStyle,
+        minZoom: this.minZoomFeatureInfo
+      });
+      this.map.addLayer(imageOutlineLayer);
+      
+      const imageLayer = new ImageLayer({
+        source: new Static({
+          url: map_dir,
+          imageExtent : extent
+        }),
+        minZoom: this.minZoomFeatureInfo
+      });
+      this.map.addLayer(imageLayer);
+
+      this.view.setCenter(getCenter(extent));
+      this.view.setZoom(this.minZoomFeatureInfo);
+    },
+
+    load_geo(){
+      const info_dir = `http://localhost:3000/info/${this.info.date}/${this.info.type}`;
+      console.log(info_dir);
+      axios.get(info_dir).then(res=>{
+        const geo = res.data;
+        console.log(geo);
+
+        let source = new VectorSource({
+          features: new GeoJSON({featureProjection:"EPSG:3857"}).readFeatures(geo),
+        });
+        console.log(source);
+
+        this.vector = new VectorLayer({
+          source: source,
+          visible: true
+        });
+        console.log(this.vector);
+        this.map.addLayer(this.vector);
+
+        console.log(geo);
+      });
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+#map{
+  height:80vh
+}
+</style>
