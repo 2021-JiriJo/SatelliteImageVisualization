fbe7db0bb26a9e91656025993524603c6cc784c8
Park Taehyun <taehyun.park.fullop@gmail.com>
2021-05-20 15:59:39 +0900

    UPDATE
    - api 요청을 diff에서 compare로 변경
    - object date 추가

--- mock/index.js
+++ mock/index.js
@@ -15,7 +15,13 @@ const __dirname = path.resolve();
 
 app.get('/object/objects', (req, res)=>{
     return res.json([
-        '자동차','비행기','도로','선박'
+        'car','plane','road','ship'
+    ]);
+});
+
+app.get('/object/:object/date', (req, res)=>{
+    return res.json([
+        '2021-01-21','2021-01-22','2021-01-23','2021-01-24'
     ]);
 });
 
@@ -33,24 +39,24 @@ app.get('/object/info/:date/:object', (req, res)=>{
     });
 });
 
-app.get('/diff/date', (req, res)=>{
+app.get('/compare/date', (req, res)=>{
     return res.json([
         '2021-01-21','2021-01-22','2021-01-23','2021-01-24'
     ]);
 });
 
-app.get('/diff/map/:date_from/:date_to/from', (req, res)=>{
-    return res.sendFile(path.join(__dirname, `./data/diff/map_${req.params.date_from}_${req.params.date_to}_1.png`));
+app.get('/compare/map/:date_from/:date_to/from', (req, res)=>{
+    return res.sendFile(path.join(__dirname, `./data/compare/map_${req.params.date_from}_${req.params.date_to}_1.png`));
 });
 
-app.get('/diff/map/:date_from/:date_to/to', (req, res)=>{
-    return res.sendFile(path.join(__dirname, `./data/diff/map_${req.params.date_from}_${req.params.date_to}_2.png`));
+app.get('/compare/map/:date_from/:date_to/to', (req, res)=>{
+    return res.sendFile(path.join(__dirname, `./data/compare/map_${req.params.date_from}_${req.params.date_to}_2.png`));
 });
 
-app.get('/diff/info/:date_from/:date_to', (req, res)=>{
-    const geojson = fs.readFileSync(`./data/diff/map_${req.params.date_from}_${req.params.date_to}.geojson`);
-    const extent1 = fs.readFileSync(`./data/diff/map_${req.params.date_from}_${req.params.date_to}_1.json`);
-    const extent2 = fs.readFileSync(`./data/diff/map_${req.params.date_from}_${req.params.date_to}_2.json`);
+app.get('/compare/info/:date_from/:date_to', (req, res)=>{
+    const geojson = fs.readFileSync(`./data/compare/map_${req.params.date_from}_${req.params.date_to}.geojson`);
+    const extent1 = fs.readFileSync(`./data/compare/map_${req.params.date_from}_${req.params.date_to}_1.json`);
+    const extent2 = fs.readFileSync(`./data/compare/map_${req.params.date_from}_${req.params.date_to}_2.json`);
 
     return res.json({
         extent1: JSON.parse(extent1),
@@ -73,7 +79,7 @@ const options = {
   apis: ['./swagger.yaml'],
 }
 const swaggerSpec = await swaggerJSDoc(options);
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/apiDocs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 app.listen(3000,()=>{
