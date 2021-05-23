5631982e0a6feed3366ef9d73d28c73f89d5735f
yuhs0383 <yuhs0383@naver.com>
2021-05-24 06:42:31 +0900

    UPDATE

--- satellite-image-visualization/src/views/Map.vue
+++ satellite-image-visualization/src/views/Map.vue
@@ -8,53 +8,29 @@
 import 'ol/ol.css';
 import Map from 'ol/Map' ;
 import View from 'ol/View';
-
 import {Image as ImageLayer, Vector as VectorLayer, Tile as TileLayer} from 'ol/layer';
-
 import {XYZ, ImageStatic as Static} from 'ol/source';
 import VectorSource from 'ol/source/Vector';
-
 import {fromLonLat} from 'ol/proj';
 import {getCenter} from 'ol/extent';
-
 import {Fill, Stroke, Style} from 'ol/style';
 import Feature from 'ol/Feature';
 import Polygon from 'ol/geom/Polygon';
-
 import GeoJSON from 'ol/format/GeoJSON';
-
 import axios from 'axios';
-
-
 export default {
   name: 'Map',
   props:{
     map_key: { type:String, default: '8xehacjegYD1ARG8VucP'},
     position: {type:Array, default:null},
     zoom: {type:Number, default: 0},
-    info: {
-      type:Object,
-      default:()=>{
-        return {
-          type:'plane',
-          date:'20210121'
-        }
-      }
-    }
   },
-  watch:{
-
-    '$route'(to){
-      
-      if(to.path == '/map'){
-
-        this.load_geo();
-        this.load_img();
-
-      }
+  watch: {
+    '$route' (to) {
+      this.info.type = to.query.type;
+      this.info.date = to.query.date;
+      //this.load_data();
     }
-
-
   },
   data(){
     return {
@@ -64,8 +40,10 @@ export default {
       imageLayer: null,
       objectInfoLayer: null,
       minZoomFeatureInfo: 10,
-
-
+      info: {
+          type:null,
+          date:null
+      }
     };
   },
   mounted() {
@@ -79,29 +57,65 @@ export default {
         }),
         visible: true
       });
-
       this.view= new View({
         center:this.position,
         zoom:this.zoom
       });
-
       this.map = new Map({
         layers: [tileLayer],
         view: this.view,
         target: 'map'
       });
       
-
+      if(this.$route.query != null){
+        
+        this.info.type = this.$route.query.type;
+        this.info.date = this.$route.query.date;
+        this.load_data();
+      }
       this.map.on('moveend', ()=>{
         this.$emit('changePosition',this.view.getCenter(), this.view.getZoom());
       });
     },
-
-    load_img(){
-      const TL = fromLonLat([126.42289638519286, 37.443824358499114]);
-      const BR = fromLonLat([126.46053314208984, 37.47451749927094]);
-      var extent = TL.concat(BR);
-
+    load_data(){
+      axios.get(`http://localhost:3000/object/info/${this.info.date}/${this.info.type}`).then(res=>{
+        let extent = res.data.extent;
+        const TL = fromLonLat([extent[0],extent[1]]);
+        const BR = fromLonLat([extent[2],extent[3]]);
+        extent = TL.concat(BR);
+        this.render_outline(TL,BR);
+        this.render_img(extent);
+        this.render_info(res.data.geojson);
+      });
+    },
+    render_info(geojson){
+        let source = new VectorSource({
+          features: new GeoJSON({featureProjection:"EPSG:3857"}).readFeatures(geojson),
+        });
+        if(this.objectInfoLayer != null)
+          this.map.removeLayer(this.objectInfoLayer);
+        this.objectInfoLayer = new VectorLayer({
+          source: source,
+          visible: true
+        });
+        this.map.addLayer(this.objectInfoLayer);
+    },
+    render_img(extent){
+      console.log(extent);
+      if(this.imageLayer != null)
+        this.map.removeLayer(this.imageLayer);
+      this.imageLayer = new ImageLayer({
+        source: new Static({
+          url: `http://localhost:3000/object/map/${this.info.date}/${this.info.type}`,
+          imageExtent : extent
+        }),
+        minZoom: this.minZoomFeatureInfo
+      });
+      this.map.addLayer(this.imageLayer);
+      this.view.setCenter(getCenter(extent));
+      this.view.setZoom(this.minZoomFeatureInfo);
+    },
+    render_outline(TL,BR){
       function getBorder(TL,BR){
         return [[
           TL,
@@ -111,7 +125,7 @@ export default {
           TL
         ]];
       }
-
+      
       function blueBoldStyle(){
         return new Style({
           stroke: new Stroke({
@@ -123,55 +137,19 @@ export default {
           }),
         })
       }
-
       if(this.imageOutlineLayer != null)
         this.map.removeLayer(this.imageOutlineLayer);
-
       this.imageOutlineLayer = new VectorLayer({
         source: new VectorSource({
-          features: [new Feature(new Polygon(getBorder(TL,BR)))]
+          features: [new Feature(new Polygon(getBorder(TL, BR)))]
         }),
         style: blueBoldStyle,
         minZoom: this.minZoomFeatureInfo
       });
       this.map.addLayer(this.imageOutlineLayer);
-      
-      if(this.imageLayer != null)
-        this.map.removeLayer(this.imageLayer);
-
-      this.imageLayer = new ImageLayer({
-        source: new Static({
-          url: `http://localhost:3000/map/${this.info.date}/${this.info.type}`,
-          imageExtent : extent
-        }),
-        minZoom: this.minZoomFeatureInfo
-      });
-      this.map.addLayer(this.imageLayer);
-
-      this.view.setCenter(getCenter(extent));
-      this.view.setZoom(this.minZoomFeatureInfo);
-    },
-
-    load_geo(){
-      axios.get(`http://localhost:3000/info/${this.info.date}/${this.info.type}`).then(res=>{
-        let source = new VectorSource({
-          features: new GeoJSON({featureProjection:"EPSG:3857"}).readFeatures(res.data),
-        });
-
-        if(this.objectInfoLayer != null)
-          this.map.removeLayer(this.objectInfoLayer);
-
-        this.objectInfoLayer = new VectorLayer({
-          source: source,
-          visible: true
-        });
-        this.map.addLayer(this.objectInfoLayer);
-      });
     }
   }
 }
-
-
 </script>
 
 <style scoped>
