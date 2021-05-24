11a36247410c653c71795f2cc93ba3792822669a
Park Taehyun <taehyun.park.fullop@gmail.com>
2021-05-25 00:22:10 +0900

    ADD 통계 기능
    - 통계 기능에 포함될 그래프를 추가
     - 객체 별 개수 그래프
     - 특정 객체의 변화 그래프
    - 해당 기능을 포함한 컴포넌트 생성

---
+++ satellite-image-visualization/src/views/Stat.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col sm="12"><BarChart></BarChart></v-col>
+      <v-col sm="12"><LineChart></LineChart></v-col>
+      <v-col sm="12">1</v-col>
+      <v-col sm="12">1</v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import BarChart from './chart/BarChart.vue';
+import LineChart from './chart/LineChart.vue';
+
+export default {
+  components: {BarChart, LineChart},
+}
+</script>
+
+<style scoped>
+#map{
+  height:80vh
+}
+</style>
---
+++ satellite-image-visualization/src/views/chart/BarChart.vue
@@ -0,0 +1,67 @@
+<script>
+
+import { Bar } from 'vue-chartjs'
+
+export default {
+  name:'BarChart',
+  extends: Bar,
+  data() {
+    return{
+      chartdata: {
+      labels: ['objects'],
+      datasets: [
+        {
+          label: 'plane',
+          backgroundColor: '#78f979',
+          data: [40]
+        },
+        {
+          label: 'car',
+          backgroundColor: '#7879f9',
+          data: [20]
+        },        
+        {
+          label: 'tank',
+          backgroundColor: '#f879f9',
+          data: [62]
+        },        
+        {
+          label: 'jeep',
+          backgroundColor: '#787979',
+          data: [33]
+        },        
+      ]
+    },
+    options: {   
+      responsive: true,
+      maintainAspectRatio: false,  
+      scales: {
+              xAxes: [{
+                  scaleLabel: {
+                      labelString: 'X axe name',
+                      fontColor:'#000000',
+                      fontSize:10},
+                  ticks: { fontColor: "#DDDDDD"}}],
+              yAxes: [{
+                  scaleLabel: {
+                      labelString: 'Y axe name',
+                      fontColor: '#000000',
+                      fontSize:10
+                  },
+                  ticks: {
+                    fontColor: "#DDDDDD",
+                    beginAtZero: true,
+                    max: 70
+                    }
+              }]
+            }
+        }
+    }
+  },
+  
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
