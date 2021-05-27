ba02b8c4602e68697e8addd7427444f53da7ae43
yuhs0383 <yuhs0383@naver.com>
2021-05-27 18:16:24 +0900

    UI update

--- satellite-image-visualization/src/plugins/vuetify.js
+++ satellite-image-visualization/src/plugins/vuetify.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import VTooltip from 'v-tooltip'
 
 Vue.use(Vuetify);
+Vue.use(VTooltip);
 
 export default new Vuetify({
+    theme: {
+        dark: true,
+      }
 });
