258d907cea9744d59ba640046dbdd139f54b6fad
yuhs0383 <yuhs0383@naver.com>
2021-05-31 08:38:55 +0900

    update

--- satellite-image-visualization/src/components/Sidebar/MainSidebar.vue
+++ satellite-image-visualization/src/components/Sidebar/MainSidebar.vue
@@ -1,29 +1,27 @@
 <template>
     <v-navigation-drawer fill-height>
         <v-divider></v-divider>
-        <v-list dense nav>
-            <v-tooltip right>
-                <template v-slot:activator="{on, attrs}">
-                    <v-list-item
-                        v-for="item in nav_items"
-                        :key="item.title"
-                        link
-                        @click="clickMenu(item.menu)"
-                        v-bind="attrs"
-                        v-on="on"
-                        >
-                        <v-list-item-icon>
-                            <v-icon>{{item.icon}}</v-icon>
+        
+        <v-list-item-content>
+            <v-list-item v-for="(item) in items" :key="item.title" >
+                <v-tooltip right>
+                    <template v-slot:activator="{on}">
+                        <v-list-item-icon
+                            v-on="on"
+                            link
+                            @click="clickMenu(item.menu)"
+                            >
+                            <v-list-item-icon>
+                                <v-icon>{{item.icon}}</v-icon>
+                            </v-list-item-icon>
                         </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title>{{item.title}}</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
                 </template>
-                <span>tooltip</span>
-            </v-tooltip>
-        </v-list>
-    <!--
+                <span>{{item.menu}}</span>
+                </v-tooltip>
+            </v-list-item>
+        </v-list-item-content>
+        
+        <!--       
         <v-list dense nav>
             <v-list-item
                 v-for="item in nav_items"
@@ -48,7 +46,7 @@
 export default ({
     data(){
         return {
-            nav_items:[
+            items:[
                 {icon: 'mdi-feature-search-outline', menu:'object' },
                 {icon: 'mdi-compare', menu:'compare'},
                 {icon: 'mdi-file-chart', menu:'home'}
